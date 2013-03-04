57bf7c61397416e4f1fa28e93571bbb941cc375f
ukyo <ukyo.web@gmail.com>
2013-03-04 15:14:43 +0900

    fix

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 var width, height;
 
-chrome.storage.local.get(['width', 'height'], function(o) {
-  chrome.app.runtime.onLaunched.addListener(function() {
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.storage.local.get(['width', 'height'], function(o) {
     chrome.app.window.create('window.html', {
       'width': o.width || 400,
       'height': o.height || 500
